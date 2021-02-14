8a887bf67558b55f741ae7d40f8805794104cb61
DESKTOP-BI837F3\kyle <nwdt3309@gmail.com>
2021-02-14 17:37:20 +0900

    [feat] proxy를 활용한 store 생성

---
+++ code/app-data/index.js
@@ -0,0 +1,27 @@
+// es6 proxy를 사용하여 store의 field가 변경될 때 마다 event fire를 할 수 있다.
+// ie 에서는 지원하지 않는다!
+
+const createStore = (eventHandlers) => new Proxy({
+    // 필요한 data field 설정
+}, {
+    // 필요한 event handler 및 trap 설정
+})
+
+export default createStore;
+
+
+// 예시
+// 쇼핑사이트에서 '상품 리스트'와 '회원 정보'에 따라 렌더링 갱신이 필요할 경우
+
+const createStore = (renderMerchandiseList, renderCustomerInfo) => new Proxy({
+    merchandiseList: [],
+    customerInfo: {}
+}, {
+    set: (obj, prop, value) => {
+        prop === 'merchandiseList' && renderMerchandiseList();
+        prop === 'customerInfo' && renderCustomerInfo();
+        obj[prop] = value;
+    }
+})
+
+export default createStore;
