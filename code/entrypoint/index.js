4eecbd9d1d71c4c94fc1bd88efd711e324600399
DESKTOP-BI837F3\kyle <nwdt3309@gmail.com>
2021-02-16 22:20:40 +0900

    entry point

--- code/entrypoint/index.js
+++ code/entrypoint/index.js
@@ -1,8 +1,5 @@
-// import module scripts you want; 
-// e.g. import {something} from './someModule';
+function initialize() {
+    // 처음 스크립트 로딩이 다 끝난 다음 일어날 일들을 처리합니다.
+}
 
-function initialize () {
-    // 처음 스크립트 로딩이 다 끝난 다음 일어날 일들을 처리한다. 페이지 초기화에 시킬 일들을 작성한다
-   }
-   
-   export default () => window.onload = initialize;
+export default () => window.onload = initialize;
